69160d39aaaaad637787f14ec6986de8cee46b7e
Bryan Becker <bryan.becker@loop.colum.edu>
2013-07-26 16:30:32 -0400

    Added information bar to bottom of each page, did some style changes as well

--- script.js
+++ script.js
@@ -3,25 +3,44 @@
 $(document).ready(function() {
 	// declare variables
 	var email = $('#email');
+	
+	// backgrounds and stuff
 	var reel = $('#reel');
 	var scorebg = $('#scorebg');
 	var sbg = $('#soundbg');
 	var mbg = $('#musicbg');
+	var bottominfo = $('#bottominfo');
+	
+	// nav items
 	var sound = $('#sound');
 	var score = $('#score');
 	var music = $('#music');
+	
+	// nav balls
 	var soundball = $('#soundball');
 	var scoreball = $('#scoreball');
 	var musicball = $('#musicball');
-	var fb = $('#fb');
+	
+	var fs = $('#fs');
 	var sc = $('#soundcloud');
 	var bc = $('#bandcamp');
 	var twitter = $('#twitter');
+	
+	// music section
+	var musicitem = $('.musicitem');
+	var musicinfo = $('.musicinfo');
 	var pillar = $('#pillarsplayer');
 	var grow = $('#growplayer');
-	var musicitem = $('.musicitem');
-	var scoreitem = $('.scoreitem');
 	var bison = $('#bisonplayer');
+	
+	// score section
+	var scoreitem = $('.scoreitem');
+	var scoreinfo = $('.scoreinfo');
+	var para = $('#paraplayer');
+	var brave = $('#braveplayer');
+	var drill = $('#drillplayer');
+	
+	var soundinfo = $('#soundinfo');
 	var reelplayer = $('#reelplayer');
 	var soundtest = $('#sound')[0];
 
@@ -41,10 +60,15 @@ $(document).ready(function() {
 	   sound.animate({color: '#ffffff'},100);
 	   music.animate({color: '#ffffff'},100);
 	   sbg.css("display","none");
+	   soundinfo.css("display","none");
+	   soundinfo.css("opacity","0");
+	   scoreinfo.css("opacity","0");
+	   musicinfo.css("opacity","0");
 	   reelplayer.css("display","none");
 	   scorebg.css("display","none");
 	   sbg.css("display","none");
 	   mbg.css("display","none");
+	   bottominfo.css("display","none");
 	   musicitem.css("display","none");
 	   scorebg.css("height","0px");
 	   score.animate({color: '#644436'},300);
@@ -52,6 +76,15 @@ $(document).ready(function() {
 	   scorebg.fadeTo(200,1);
 	   scorebg.animate({height:'300px'});
 	   scoreitem.delay(500).fadeTo(500,1);
+	   bottominfo.css("height","0px");
+	   bottominfo.css("display","block");
+	   bottominfo.delay(200).animate({backgroundColor: '#644436'},500);
+	   bottominfo.animate({height:'126px'},300);
+	   scoreinfo.delay(800).fadeTo(300,1);
+    });
+    
+    scoreitem.click(function(event) {
+        alert(event.target.id);
     });
     
     //sound nav functions
@@ -69,12 +102,23 @@ $(document).ready(function() {
     {
     	music.animate({color: '#ffffff'},100);
     	score.animate({color: '#ffffff'},100);
+    	musicitem.css("display","none");
+    	soundinfo.css("display","none");
+    	soundinfo.css("opacity","0");
+    	bottominfo.css("display","none");
+    	scoreinfo.css("display","none");
+    	musicinfo.css("display","none");
     	scorebg.css("display","none");
     	mbg.css("display","none");
     	sbg.css("display","none");
     	sound.animate({color: '#644436'},300);
 	    sbg.fadeTo(200,1);
 	    reelplayer.css("display","block");
+	    bottominfo.css("height","0px");
+	    bottominfo.css("display","block");
+	    bottominfo.animate({backgroundColor: '#644436'},50);
+	    bottominfo.animate({height:'70px'},300);
+	    soundinfo.delay(300).fadeTo(300,1);
     });
     
     //music nav functions
@@ -90,6 +134,11 @@ $(document).ready(function() {
     {
     	score.animate({color: '#ffffff'},100);
     	sound.animate({color: '#ffffff'},100);
+    	scoreinfo.css("display","none");
+    	soundinfo.css("display","none");
+    	soundinfo.css("opacity","0");
+    	musicinfo.css("opacity","0");
+       	bottominfo.css("display","none");
     	scorebg.css("display","none");
     	reelplayer.css("display","none");
     	sbg.css("display","none");
@@ -100,6 +149,12 @@ $(document).ready(function() {
     	mbg.fadeTo(200,1);
 	    mbg.animate({height: '300px'});
 	    musicitem.delay(500).fadeTo(500,1);
+	    bottominfo.css("height","0px");
+	    bottominfo.css("display","block");
+	    bottominfo.delay(200).animate({backgroundColor: '#644436'},500);
+	    bottominfo.animate({height:'126px'},300);
+	    musicinfo.delay(800).fadeTo(300,1);
+	    
     });
     
     //email functions
@@ -113,13 +168,13 @@ $(document).ready(function() {
     });
     
     //bottom bar animations
-    fb.mouseenter(function()
+    fs.mouseenter(function()
     {
-		fb.animate({backgroundColor: '#7BF0CA'},200);
+		fs.animate({backgroundColor: '#7BF0CA'},200);
 	});
-	fb.mouseleave(function()
+	fs.mouseleave(function()
 	{
-		fb.animate({backgroundColor: '#90DADA'},200)
+		fs.animate({backgroundColor: '#90DADA'},200)
 	})
 	
 	sc.mouseenter(function()
